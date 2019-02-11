43fec1c0f769f48e4143405ee8d5b7a8fe7876d5
Charlhye <carlosbmtz@gmail.com>
2019-02-11 13:04:35 -0600

    Main Activity

---
+++ app/src/main/java/mx/charlhyemartinez/challenge/HomeScreen.java
@@ -0,0 +1,37 @@
+package mx.charlhyemartinez.challenge;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class HomeScreen extends AppCompatActivity {
+
+    TextView cp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+        cp = findViewById(R.id.cp);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 500 && resultCode == RESULT_CANCELED){
+            Toast.makeText(this, "EL CÓDIGO POSTAL NO ES VÁLIDO",Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    public void search(View view){
+        Intent intent = new Intent(this, ListaActivity.class); //Aqui va la clase de Martn
+        intent.putExtra("CP", Integer.parseInt(cp.getText().toString()));
+        startActivityForResult(intent, 500);
+    }
+}
